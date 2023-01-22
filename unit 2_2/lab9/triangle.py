b878ea301eaa0aafd403e0eba1296f7dc93d3735
pikoloda <pikoloda@student.wszib.edu.pl>
2023-01-22 12:08:50 +0100

    Przesyłam zadania lab9 - operacje logiczne i bitowe

--- unit 2_2/lab9/triangle.py
+++ unit 2_2/lab9/triangle.py
@@ -12,7 +12,7 @@ if (a + b > c and a + c > b and b + c > a):
         print("równoramienny", end=" ")
     else:
         print("różnoramienny", end=" ")
-B
+
     # sprawdzanie rodzaju trójkata ze wgledu na kąty
 
     if (a ** 2 + b ** 2 == c ** 2) or (b ** 2 + c ** 2 == a ** 2) or (a ** 2 + c ** 2 == b ** 2):
