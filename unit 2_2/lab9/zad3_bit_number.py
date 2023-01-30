672ada24e98498b043acc8110127a4c4f3c2d80c
kolod <Pikoloda@student.wzsib.edu.pl>
2023-01-30 20:23:44 +0100

    Poprawa zadań lab9: - zad1 - dodanie opisu, poprawa estetyki - zad2 - dodanie opisu, dodanie warunku, który wyprintowano przy odpowiedzi - zad3 - dodanie opisu, poprawa estetyki

--- unit 2_2/lab9/zad3_bit_number.py
+++ unit 2_2/lab9/zad3_bit_number.py
@@ -1,7 +1,6 @@
-# 3. Napisz program wyznaczający wartość n-tego bitu dla dowolnej liczby
+# zad3.
+# Napisz program wyznaczający wartość n-tego bitu dla dowolnej liczby
 # całkowitej. Bity liczymy od 0, od najmniej znaczącego bitu
-
-# liczymy bity od najmniej znaczących # 01001 -> w tym wypadku od prawej -> 1
 
 # 01001 - przykładowa liczba
 # 00001 - maska
@@ -14,11 +13,5 @@ n = int(input("Podaj nr bitu: "))
 mask = 1 << n
 result = number & mask
 
-bit = int(bool(result)) # daje nam wartość bita
+bit = int(bool(result))
 print("Bit na pozycji", n, "dla liczby", number, "wynosi", bit,".")
-
-
-print("{:08b}".format(number))
-print("{:08b}".format(mask))
-print(("-"*8))
-print("{:08b}".format(result))
