33c3c79677c6b90654bcc30bbbbfb3a574770e95
kolod <Pikoloda@student.wzsib.edu.pl>
2023-03-04 20:49:32 +0100

    lab11 - zmiana etytyczna treści zadań zad1,zad2,zad3

--- unit 3_1/lab11/zad1_lotto.py
+++ unit 3_1/lab11/zad1_lotto.py
@@ -1,4 +1,5 @@
- # 1. Napisz skrypt symulujący grę losową:
+ # Zad1.
+ # Napisz skrypt symulujący grę losową:
  # • użytkownik obstawia 6 liczb z 49,
  # • program losuje 6 liczb z 49,
  # • użytkownik dostaje informacje o ilości trafień.
--- unit 3_1/lab11/zad2_sorted_numbers.py
+++ unit 3_1/lab11/zad2_sorted_numbers.py
@@ -1,4 +1,5 @@
-# 2. Napisz skrypt pobierający od użytkownika serię liczb całkowitych, a
+# Zad2.
+# Napisz skrypt pobierający od użytkownika serię liczb całkowitych, a
 # następnie wyświetl je w kolejności malejącej pozbywając się wcześniej
 # duplikatów.
 
